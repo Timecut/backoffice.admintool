33f6c03e8b38c184aaf80f2099f2904db6e1fa9d
CalkaRob <calka.rob@gmail.com>
2025-01-16 10:38:53 +0100

    up basic frontend admintool app

--- frontend/frontend_admintool/src/App.tsx
+++ frontend/frontend_admintool/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>backoffice.admintool app</h1>
+      <h1>frontend_admintool app</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
